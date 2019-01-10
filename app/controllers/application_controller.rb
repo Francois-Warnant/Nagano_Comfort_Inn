c1333ca7d1337a38a87f24ff6d450c19a0bfe1e1
Francois Warnant <warnant_francois@hotmail.com>
2019-01-10 11:34:30 -0500

    Ajout migration remove_name_from_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
+  #def after_sign_in_path_for(resource)
+    #current_user_path
+  #end
+
   protect_from_forgery
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,9 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20190108190549) do
+ActiveRecord::Schema.define(:version => 20190110160206) do
 
   create_table "users", :force => true do |t|
-    t.string   "firstname"
-    t.string   "lastname"
     t.string   "role_id"
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
